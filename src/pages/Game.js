c8c86cb00fe79341dd1c2a18dbcdaf8f4a5bff66
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-11 19:36:48 +0000

    Styled game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { StyledGame, StyledScore, StyledTimer, StyledChar } from '../styled/Game'
+import { Strong } from '../styled/Random'
 
 export default function Game(){
     return (
-        <div>
-            <h1>Game</h1>
-        </div>
+        <StyledGame>
+            <StyledScore>Score: <Strong>0</Strong></StyledScore>
+            <StyledChar>A</StyledChar>
+            <StyledTimer>Time: <Strong>00: 000</Strong></StyledTimer>
+        </StyledGame>
         );
 }
