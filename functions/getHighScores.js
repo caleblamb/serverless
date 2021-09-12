598d79c504e8e3b0f34a02e70fc6980921104f3d
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-12 23:32:24 +0000

    fixed High Scores function

--- functions/getHighScores.js
+++ functions/getHighScores.js
@@ -7,7 +7,10 @@ const base = Airtable.base('appvsJrTK9VfH8jNT');
 const table = base.table('scores');
 exports.handler = async (event) => {
     const records = await table.select().firstPage();
-    const formattedRecords = records.map;
+    const formattedRecords = records.map((record) => ({
+        id: record.id,
+        fields: record.fields
+    }));
     return {
         statusCode: 200,
         body: JSON.stringify(formattedRecords),
