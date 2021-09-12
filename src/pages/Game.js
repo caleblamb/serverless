c4e84c3369651ee7898aa909e2d403775bcc2279
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-12 20:36:11 +0000

    added an exclamation

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,19 @@
-import React, {useState, useEffect} from "react";
-import { StyledGame, StyledScore, StyledTimer, StyledChar } from '../styled/Game'
-import { Strong } from '../styled/Random'
+import React, {useState, useEffect, useCallback} from "react";
+import { StyledGame, StyledScore, StyledTimer, StyledChar } from '../styled/Game';
+import { Strong } from '../styled/Random';
+import { useScore } from '../contexts/ScoreContext';
 
 export default function Game({history}){
-    const [score, setScore] = useState(1);
-    const MAX_SECONDS = 90;
+    const [score, setScore] = useScore();
+    const MAX_SECONDS = 5;
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const [currentCharacter, setCurrentCharacter] = useState('');
     const [ms, setMs] = useState(0);
     const [seconds, setSeconds] = useState(MAX_SECONDS);
     
     useEffect(() => {
+        setRandomCharacter();
+        setScore(0);
         const currentTime = new Date();
         const interval = setInterval( () => updateTime(currentTime),1);
         return() => clearInterval(interval);
@@ -38,18 +43,37 @@ export default function Game({history}){
         }
     }, [seconds, ms, history]);
     
-    const keyUpHandler = (e) => {
-        console.log(e.key);
-    };
+    const keyUpHandler = useCallback(
+            (e) => {
+            if (e.key === currentCharacter){
+                setScore((prevScore) => prevScore + 1);
+            }
+            else {
+                if(score > 0){
+                    setScore((prevScore) => prevScore -1);
+                }
+            }
+            setRandomCharacter();
+        },
+        [currentCharacter]
+    );
     
     useEffect(() => {
         document.addEventListener('keyup', keyUpHandler);
-    },[]);
+        return () => {
+            document.removeEventListener('keyup', keyUpHandler);
+        }
+    },[keyUpHandler]);
+    
+    const setRandomCharacter = () => {
+      const randomInt = Math.floor(Math.random() * 36);
+      setCurrentCharacter(characters[randomInt]);
+    };
     
     return (
         <StyledGame>
             <StyledScore>Score: <Strong>{score}</Strong></StyledScore>
-            <StyledChar>A</StyledChar>
+            <StyledChar>{currentCharacter}</StyledChar>
             <StyledTimer>Time: <Strong>{seconds}: {ms}</Strong></StyledTimer>
         </StyledGame>
         );
