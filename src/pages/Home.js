b4fc37f7eae557d9f01438067ef53d3a2b7cc03e
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-13 13:47:11 +0000

    fixed home page copy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ import { Accent, StyledTitle } from '../styled/Random'
 export default function Home(){
     return (
         <div>
-            <StyledTitle>Ready to type?!</StyledTitle>
-            <CTA to="/game">Click or type <Accent>'s'</Accent>to start playing!</CTA>
+            <StyledTitle>Ready to type?</StyledTitle>
+            <CTA to="/game">Click or type <Accent>'s'</Accent> to start playing!</CTA>
         </div>
         );
 }
